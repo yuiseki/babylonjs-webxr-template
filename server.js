41dcd52cf020dbbd81c8622ae34e0bc85eb862cb
yuiseki <yuiseki@gmail.com>
2020-08-24 20:01:36 +0900

    fix advanced and using webpack

--- server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const open = require('open');
-const express = require('express');
-const app = express();
-app.use(express.static('./'));
-app.listen(8001, async ()=> {
-  console.log('vr gui started http://localhost:8001/');
-  await open('http://localhost:8001/');
-});
--- src/advanced.ts
+++ src/advanced.ts
@@ -1,6 +1,6 @@
 import * as BABYLON from '@babylonjs/core';
 import * as GUI from '@babylonjs/gui';
-import { WebXRProfiledMotionController, WebXRControllerPhysics, WebXRControllerPointerSelection, WebXRMotionControllerTeleportation } from '@babylonjs/core';
+import { WebXRProfiledMotionController, WebXRControllerPhysics, WebXRControllerPointerSelection, WebXRMotionControllerTeleportation, WebXRInputSource } from '@babylonjs/core';
 
 var canvas:HTMLCanvasElement = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
@@ -17,7 +17,7 @@ var createScene = async function (engine) {
     var environment = scene.createDefaultEnvironment();
     environment.setMainColor(BABYLON.Color3.FromHexString("#74b9ff"));
     // 地面
-    var ground1 = BABYLON.MeshBuilder.CreateGround("ground1", {width:50, height:50, subdivisions:2}, scene);
+    var ground1 = BABYLON.MeshBuilder.CreateGround("ground1", {width:50, height:50, subdivisions:4}, scene);
     
     // ログ出力用のTextBlock
     var planeX = BABYLON.MeshBuilder.CreatePlane("planeX", {size: 1});
@@ -62,20 +62,26 @@ var createScene = async function (engine) {
     headerZ.fontSize = "60";
     panelZ.addControl(headerZ);
 
-    
+    const createRandomVector3 = () => {
+        // 1から5までのfloatをランダムに得る
+        const x = (Math.round(Math.random())*2-1)*Math.random()*5;
+        const y = Math.random()*5;
+        const z = (Math.round(Math.random())*2-1)*Math.random()*5;
+        return new BABYLON.Vector3(x, y, z);
+    }
 
-    // テスト用のオブジェクト
-    var box1 = BABYLON.MeshBuilder.CreateBox("box1", {size:1}, scene);
-    box1.position = new BABYLON.Vector3(1, 1, 1);
-    // 殴ると回転する
-    box1.onCollideObservable.add((eventData, eventState)=>{
-        box1.addRotation(box1.rotation.x+1, box1.rotation.y+1, box1.rotation.z+1);
-    });
+    // テスト用のオブジェクトをランダムに配置する
+    const count = Math.round(Math.random()*20);
+    let i = 0;
+    while( i < count){
+        i++;
+        var box = BABYLON.MeshBuilder.CreateBox("box"+i, {size:0.0001}, scene);
+        box.position = createRandomVector3();
 
-    var sphere1 = BABYLON.MeshBuilder.CreateSphere("sphere1", {diameter:2}, scene);
-    sphere1.position = new BABYLON.Vector3(-1, 1, 1); 
+        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere"+i, {diameter:0.0001}, scene);
+        sphere.position = createRandomVector3();
+    }
 
-    
     // XRが動く環境かチェック
     const xrSupported = await BABYLON.WebXRSessionManager.IsSessionSupportedAsync('immersive-vr');
     console.log("xrSupported: "+xrSupported);
@@ -89,19 +95,58 @@ var createScene = async function (engine) {
     });
 
     // 掴む, 離す
-    xrHelper.input.onControllerAddedObservable.add((inputSource)=>{
-        inputSource.onMotionControllerInitObservable.add((controller:WebXRProfiledMotionController)=>{
-            const squeezeComponent = controller.getComponent("xr-standard-squeeze");
-            squeezeComponent.onButtonStateChangedObservable.add((component)=>{
+    try {
+        const setGrab = (inputSource:WebXRInputSource, controller:WebXRProfiledMotionController) => {
+            let grabbedMesh:BABYLON.AbstractMesh = null;
+            let originalPosition = null;
+            // トリガーで掴む
+            const triggerComponent = controller.getComponent("xr-standard-trigger");
+            triggerComponent.onButtonStateChangedObservable.add((component)=>{
                 if(component.value > 0.8 && component.pressed){
-                    if(xrHelper.pointerSelection.getMeshUnderPointer){
-                        const mesh = xrHelper.pointerSelection.getMeshUnderPointer(controller.profileId);
-                        mesh.position = controller.rootMesh.position;
+                    // トリガーを握った
+                    if(grabbedMesh === null){
+                        // 何も掴んでいない状態
+                        // 掴みたいMeshを得る
+                        // MEMO: scene.meshUnderPointer を使うと左右のコントローラーの区別ができない
+                        // MEMO: xrHelper.pointerSelection.getMeshUnderPointer(controller.profileId); は常に null
+                        grabbedMesh = xrHelper.pointerSelection.getMeshUnderPointer(inputSource.uniqueId);
+                        console.log("grabbedMesh: "+grabbedMesh);
+                        if(grabbedMesh !== null){
+                            // 掴みたいMeshの元のPositionを覚えておく
+                            if(originalPosition === null){
+                                originalPosition = new BABYLON.Vector3(grabbedMesh.position.x, grabbedMesh.position.y, grabbedMesh.position.z);
+                            }
+                            grabbedMesh.position = controller.rootMesh.absolutePosition;
+                        }
+                    }else{
+                        // 既になにか掴んでいる状態
+                        // 掴んでる間はコントローラーと同じ座標に追随させる
+                        grabbedMesh.position = controller.rootMesh.absolutePosition;
+                    }
+                }else{
+                    // 離した
+                    // 掴んでいたMeshを元のPositionに戻す
+                    if(grabbedMesh !== null && originalPosition !== null){
+                        grabbedMesh.position = originalPosition;
+                        originalPosition = null;
+                        grabbedMesh = null;
                     }
                 }
             });
+        };
+        xrHelper.input.onControllerAddedObservable.add((inputSource)=>{
+            console.log("inputSource: ", inputSource);
+            inputSource.onMotionControllerInitObservable.add((controller:WebXRProfiledMotionController)=>{
+                if(!xrHelper.pointerSelection.getMeshUnderPointer){
+                    return;
+                }
+                console.log("controller: ", controller);
+                setGrab(inputSource, controller);
+            });
         });
-    });
+    } catch (error) {
+        console.error(error);
+    }
 
     return scene;
 };
